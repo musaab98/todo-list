b73ea325596e8ebde292162337ce8f4f62892c18
Musaab Elsheikh <musaab@Musaabs-MacBook-Air.local>
2023-09-09 09:27:26 -0400

    build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'todo-list',
+  base: 'todo-list',build: {
+    outDir: 'build', // Ensure this matches your actual build directory
+    // ...
+  },
   publicPath: '',
   plugins: [react()],
 })
